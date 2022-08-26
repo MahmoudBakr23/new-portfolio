0469236fe71067064a6ef0eff6ef424488b12422
Mahmoud Bakr <mbakr6821@gmail.com>
2022-08-26 19:50:02 +0200

    Set up the routing system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Home from "./Components/Home";
 import About from "./Components/About";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App" style={{margin: 0}}>
-      <Home />
-      <About />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/aboutme" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
