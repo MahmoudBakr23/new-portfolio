6044a033d0eb17db4c326d92b6c5ce521ecdd6ad
Mahmoud Bakr <mbakr6821@gmail.com>
2022-09-02 15:17:48 +0200

    Add contact form to the Contact Component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,12 +3,13 @@ import { Container, LinesCont, OneLine } from "./Styles/Home.styled";
 import { EducationWrapper, Time, University, College, EducationItem } from "./Styles/Education.styled";
 import { TechDiv, TechItem, Link } from "./Styles/Experience.styled";
 import { Heading } from "./Styles/About.styled";
+import { Name, NameDiv, FormBtn, FormDiv, Message } from "./Styles/Contact.styled";
 
 const Contact = () => {
     return(
         <div>
             <Container right>
-            <EducationWrapper flex={"center"} margin={"10rem 0 0 10rem"}>
+            <EducationWrapper flex={"center"} margin={"10rem 0 0 8rem"}>
                 <Heading>Contact Me</Heading>
                 <EducationItem>
                     <Time>Phone : +20 1146 467 077</Time>
@@ -20,7 +21,22 @@ const Contact = () => {
                     </TechDiv>
                 </EducationItem>
             </EducationWrapper>
-                <LinesCont right={"100"} top={"0"} hght={"40%"} mar={"0 10rem"}>
+            <EducationWrapper flex={"center"} margin={"8rem 8rem 0 0"}>
+                <FormDiv action="https://formspree.io/f/myylerlb" method="POST">
+                    <NameDiv>
+                        <Name name="first Name" placeholder="First Name" width={"100%"} required/>
+                        <Name name="second Name" placeholder="Last Name" margin={"0 0 0 1rem"} width={"100%"} required/>
+                    </NameDiv>
+                    <Name type="email" name="email" placeholder="Email" width={"100%"} margin={"1rem 0 0 0"} required/>
+                    <Name name="subject" placeholder="Subject" width={"100%"} margin={"1rem 0 0 0"} required/>
+                    <Message name="message" placeholder="Write your message here..." rows={"5"} required/>
+                    <NameDiv>
+                        <FormBtn name="Button" type="submit">SUBMIT</FormBtn>
+                        <Time style={{width:"100%", margin:"1rem 0 0 0"}}>Thank you for submitting!</Time>
+                    </NameDiv>
+                </FormDiv>
+            </EducationWrapper>
+                <LinesCont right={"100"} top={"0"} hght={"40%"} mar={"0 8rem"}>
                     <OneLine />
                     <OneLine />
                     <OneLine />
---
+++ src/Components/Styles/Contact.styled.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+const FormDiv = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-self: flex-end;
+    align-items: flex-start;
+    justify-content: start;
+    width: 100%;
+    height: 100%;
+`
+
+const NameDiv = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+`
+
+const Name = styled.input`
+    border: 0;
+    outline: 0;
+    padding: 1rem;
+    font-size: 1rem;
+    font-weight: 600;
+    color: white;
+    width: ${props => props.width};
+    margin: ${props => props.margin};
+    background-color: #333;
+    letter-spacing: 3px;
+    &:focus {
+        border-bottom: 1px solid white;
+    }
+`
+
+const Message = styled.textarea`
+    border: 0;
+    outline: 0;
+    padding: 1rem;
+    font-size: 1rem;
+    font-weight: 300;
+    margin: 1rem 0 0 0;
+    color: white;
+    width: 100%;
+    background-color: #333;
+    &:active {
+        border-bottom: 1px solid white;
+    }
+`
+
+const FormBtn = styled.button`
+    padding: 1rem;
+    margin: 1rem 1rem 0 0;
+    width: 100%;
+    font-size: 1.3rem;
+    font-weight: 500;
+    background-color: white;
+    color: #333;
+    border: 1px solid white;
+    outline: 0;
+    background-color: #fff;
+`
+
+export { FormBtn, FormDiv, Name, Message, NameDiv };
